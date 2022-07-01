4bad8ad9460f0d64fb01b2bac1769ae942aa148b
ProfessorAtomicManiac <cyific@gmail.com>
2022-07-01 12:25:40 -0700

    changed buttons classes, made stuff scroll smoother

--- visual.py
+++ visual.py
@@ -20,44 +20,40 @@ class Background():
         self.screen = screen
         self.background = pygame.Surface((width, height))
         self.background.fill("#0D1B2A")
+        self.screen_change = True
 
     def update(self):
         self.window.blit(self.background, (0, 0))
 
     def switch_screen(self, screen):
         self.screen = screen
-
+    
 class Text(pygame.sprite.Sprite):
 
-    def __init__(self, window, text, pos, font, color, screen):
+    def __init__(self, window, text, coords, font, color, screen):
         super().__init__()
         self.window = window
         self.image = font.render(text, True, color)
-        self.rect = self.image.get_rect(center = pos)
+
+        self.rect = self.image.get_rect(center = (window.window.get_size()[0] + 1000, coords[1]))
         self.screen = screen
-    
+        self.accel = 0
+        self.coords = coords
 
     def destroy(self):
         if (self.rect.x <= -600):
             self.kill()
 
     def update(self):
-        if (self.screen != self.window.screen and self.screen != Screen.NONE):
-            self.rect.x -= 15
+        if ((self.screen != self.window.screen and self.screen != Screen.NONE) or self.rect.x > self.coords[0]):
+            self.accel += -1
+            self.rect.x += self.accel
+        else:
+            self.accel = 0
+            self.rect = self.image.get_rect(center = self.coords)
         self.destroy()
 
-# https://www.clickminded.com/button-generator/
 class Button(pygame.sprite.Sprite):
-    '''Button Creation
-    Images list should have at least two images, one for hover and one for default
-    The first one should be default, the second be hover
-    Generally default: #415A77
-              hovering: #778DA9
-              text: #E0E1DD
-              Width: 200, Height: 50, Corners Radius: 11
-              Bold, Size: 26, 
-              Font: Ubuntu (haha get it?)
-    '''
     def __init__(self, images, coords, function, screen, background):
         super().__init__()
         self.images = images
@@ -86,26 +82,85 @@ class Button(pygame.sprite.Sprite):
         self.player_input()
         self.destroy()
 
+class TextButton(pygame.sprite.Sprite):
+    # https://www.clickminded.com/button-generator/
+    '''Button Creation
+    Images list should have at least two images, one for hover and one for default
+    The first one should be default, the second be hover
+    Generally default: #415A77
+              hovering: #778DA9
+              text: #E0E1DD
+              Width: 200, Height: 50, Corners Radius: 11
+              Bold, Size: 26, 
+              Font: Ubuntu (haha get it?)
+    The window should be the background class
+    '''
+    def __init__(self, text, coords, function, screen, window):
+        super().__init__()
+        ubuntu_font = pygame.font.Font("Fonts/Ubuntu-Bold.ttf", 26)
+        self.text = ubuntu_font.render(text, True, "#E0E1DD")
+        self.image = pygame.image.load("Button/button.png").convert_alpha()
+        self.coords = coords
+        self.rect = self.image.get_rect(center = (window.window.get_size()[0] + 200, coords[1]))
+        self.textRect = self.text.get_rect(center = (window.window.get_size()[0] + 200, coords[1]))
+        self.function = function
+        self.screen = screen
+        self.window = window
+        self.accel = 0
+        self.width = 200
+        self.height = 50
+
+    def player_input(self):
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            if pygame.mouse.get_pressed()[0]:
+                self.function(self.window)
+            else:
+                self.image = pygame.image.load("Button/button_hovering.png").convert_alpha()
+        else:
+            self.image = pygame.image.load("Button/button.png").convert_alpha()
+
+    def destroy(self):
+        if (self.rect.x <= -200):
+            self.kill()
+
+    def slide(self):
+        if ((self.screen != self.window.screen and self.screen != Screen.NONE) or self.rect.x + self.width/2 > self.coords[0]):
+            self.accel += -1
+            self.rect.x += self.accel
+            self.textRect.x += self.accel
+        else:
+            self.accel = 0
+            self.rect = self.image.get_rect(center = self.coords)
+            self.textRect = self.text.get_rect(center = self.coords)
+
+    def update(self):
+       
+        #self.window.window.blit(self.text, (self.rect.x + self.width/2, self.rect.y + self.height/2))
+        self.window.window.blit(self.text, self.textRect)
+        self.player_input()
+        self.slide()
+        self.destroy()
+    
 class DisplayScreen():
     def display_main_menu(window, screenGroup):
         ubuntu_font = pygame.font.Font("Fonts/Ubuntu-Bold.ttf", 50)
+
         screenGroup.empty()
         screenGroup.add(Text(window, "Algorithm Visualizer", (window.window.get_size()[0]/2, 100), ubuntu_font, "#E0E1DD", Screen.MAIN_MENU))
-        sorting_button_1 = pygame.image.load('Button/SortingButtons/button_sorting.png').convert_alpha()
-        sorting_button_2 = pygame.image.load('Button/SortingButtons/button_sorting_hover.png').convert_alpha()
-        screenGroup.add(Button((sorting_button_1, sorting_button_2), ((window.window.get_size()[0]/2), 200), DisplayScreen.pressed_sorting, Screen.MAIN_MENU, window))
-        graph_button_1 = pygame.image.load('Button/GraphButtons/button_graph.png').convert_alpha()
-        graph_button_2 = pygame.image.load('Button/GraphButtons/button_graph_hover.png').convert_alpha()
-        screenGroup.add(Button((graph_button_1, graph_button_2), ((window.window.get_size()[0]/2), 270), DisplayScreen.pressed_graph, Screen.MAIN_MENU, window))
+        screenGroup.add(TextButton("Sorting", ((window.window.get_size()[0]/2), 200), DisplayScreen.pressed_sorting, Screen.MAIN_MENU, window))
+        screenGroup.add(TextButton("Graphs", ((window.window.get_size()[0]/2), 270), DisplayScreen.pressed_graph, Screen.MAIN_MENU, window))
         options_button_1 = pygame.image.load('Button/OptionButtons/options.png').convert_alpha()
         options_button_1 = pygame.transform.rotozoom(options_button_1, 0, 0.1)
         options_button_2 = pygame.image.load('Button/OptionButtons/options_hover.png').convert_alpha()
         options_button_2 = pygame.transform.rotozoom(options_button_2, 0, 0.1)
         screenGroup.add(Button((options_button_1, options_button_2), ((window.window.get_size()[0])-40, 40), DisplayScreen.pressed_options, Screen.NONE, window))
+        
     def pressed_sorting(window):
         window.switch_screen(Screen.SORTING_SCREEN)
+        window.screen_change = True
     def pressed_graph(window):
         window.switch_screen(Screen.GRAPH_MENU)
+        window.screen_change = True
     def pressed_options():
         pass
 
@@ -113,7 +168,6 @@ def main(fps):
     pygame.init()
     window = Background(800, 400, Screen.MAIN_MENU)
     screenGroup = pygame.sprite.Group()
-    screen_change = True
 
     while True:
         window.update()
@@ -123,15 +177,15 @@ def main(fps):
             if event.type == pygame.VIDEORESIZE:
                 width, height = event.size
                 window = Background(width, height)
-                screen_change = True
+                window.screen_change = True
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
         # apparentally python has no switch cases?
         # either update to python 3.10 or create a dictionary
-        if (window.screen == Screen.MAIN_MENU and screen_change):
+        if (window.screen == Screen.MAIN_MENU and window.screen_change):
             DisplayScreen.display_main_menu(window, screenGroup)
-            screen_change = False
+            window.screen_change = False
         screenGroup.draw(window.window)
         screenGroup.update()
         pygame.display.update()
